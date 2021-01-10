87fbf347e3124ec72b1aca1daf82f0840eb67a04
mhussein15 <mhalfakih15@gmail.com>
2021-01-10 15:19:59 +0300

    Tasked Finished 2

--- calculator.js
+++ calculator.js
@@ -2,16 +2,18 @@
 
 // Write if-statements here
 
-
 const num1 = prompt("Enter First Number");
-const opt = prompt("Enter Operation");
+const opt = prompt("Enter One of those Operations:- [ + , * , - , / , % ]");
 const num2 = prompt("Enter Second Number");
 
-
-if(opt === "+"){
-  console.log(`${num1}+${num2} = ${parseInt(num1) +parseInt (num2)}`);
-}else if(opt === "-"){
-  console.log(`${num1}-${num2} = ${parseInt(num1) -parseInt (num2)}`);
-}else if(opt === "*"){
-  console.log(`${num1}*${num2} = ${parseInt(num1) *parseInt (num2)}`);
+if (opt === "+") {
+  console.log(`${num1}+${num2} = ${parseInt(num1) + parseInt(num2)}`);
+} else if (opt === "-") {
+  console.log(`${num1}-${num2} = ${parseInt(num1) - parseInt(num2)}`);
+} else if (opt === "*") {
+  console.log(`${num1}*${num2} = ${parseInt(num1) * parseInt(num2)}`);
+} else if (opt === "/") {
+  console.log(`${num1}/${num2} = ${parseInt(num1) / parseInt(num2)}`);
+} else if (opt === "%") {
+  console.log(`${num1}%${num2} = ${parseInt(num1) % parseInt(num2)}`);
 }
