173f261921620c171389c6d6b7dfabafb6c932df
mhussein15 <mhalfakih15@gmail.com>
2021-01-10 15:16:46 +0300

    Tasked Finished

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
 
 // Write if-statements here
+
+
+const num1 = prompt("Enter First Number");
+const opt = prompt("Enter Operation");
+const num2 = prompt("Enter Second Number");
+
+
+if(opt === "+"){
+  console.log(`${num1}+${num2} = ${parseInt(num1) +parseInt (num2)}`);
+}else if(opt === "-"){
+  console.log(`${num1}-${num2} = ${parseInt(num1) -parseInt (num2)}`);
+}else if(opt === "*"){
+  console.log(`${num1}*${num2} = ${parseInt(num1) *parseInt (num2)}`);
+}
